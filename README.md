92aa3cd033da10ef02cae62b99cf524cdde6b867
Zoe Heins <zheins@leapfrogonline.com>
2018-03-28 13:20:16 -0500

    setup and readme update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # soundcloud-scraper
+
+A work in progress python package for scraping track titles and usernames from soundcloud playlists
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='soundcloud-scraper',
+      version='0.1',
+      description='Scraper for compiling SoundCloud tracks',
+      url='http://github.com/zoeheins/soundcloud-scraper',
+      author='Zoe Heins',
+      author_email='zcheins@gmail.com',
+      license='MIT',
+      packages=['soundcloud-scraper'],
+      zip_safe=False)
