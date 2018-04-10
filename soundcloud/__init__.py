7654d69fd74f311152b6587caa60ffebf981fba7
Zoe Heins <zheins@leapfrogonline.com>
2018-04-10 14:32:49 -0500

    different methods for playlist and likes

--- soundcloud/__init__.py
+++ soundcloud/__init__.py
@@ -1,6 +1,11 @@
 from .scraper import scrape
-from .parser import parse
+from .parser import SoundCloudParser
 
-def get_tracks():
-    html = scrape()
-    return parse(html)
+def get_playlist_tracks(playlist_url):
+    html = scrape(playlist_url, 30000)
+    return SoundCloudParser(html).parse_playlist()
+
+def get_likes(username):
+    url = 'https://soundcloud.com/{}/likes'.format(username)
+    html = scrape(url, 1100000)
+    return SoundCloudParser(html).parse_likes()
